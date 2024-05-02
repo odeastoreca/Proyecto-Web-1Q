24c911b4c70997ed37b1d601bfda6ae40e525a12
odeastoreca <149507997+odeastoreca@users.noreply.github.com>
2024-05-02 09:24:45 -0300

    push3

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo")
+
+let btnCalcular=document.getElementById("btnCalcular")
+btnCalcular.addEventListener("click",clickcalcular)
+
+function clickcalcular(){
+    
+    let txtestatura=document.getElementById("estatura")
+    let txtpeso=document.getElementById("peso") 
+    let estatura=txtestatura.value
+    let peso=txtpeso.value
+    if (isNaN(estatura) || isNaN(peso)) {
+        alert("Por favor, ingrese valores válidos.")}
+    else{
+        let imc=peso/(estatura*estatura)
+        alert( "Su IMC es: " + Math.round(imc));}
+}
+
+
+
+

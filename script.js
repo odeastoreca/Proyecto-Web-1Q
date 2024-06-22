ac23ecc95b74f47db6bdedffc5f956f73be2df26
odeastoreca <149507997+odeastoreca@users.noreply.github.com>
2024-06-21 21:08:32 -0300

    push

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function clickcalcular(){
 }
 
 
-
+// Contador calorias
 function calcularCalorias() {
     var genero = document.getElementById('genero').value;
     var edad = parseInt(document.getElementById('edad').value);
@@ -51,6 +51,29 @@ function calcularCalorias() {
     var resultadoElement = document.getElementById('resultado');
     resultadoElement.innerHTML = 'Tus necesidades calóricas diarias son aproximadamente ' + calorias + ' calorías.';
   }
+  
+  
+// Generador de rutinas
+
+  
+function generarRutina() {
+    const rutinas = {
+        1: "Día 1: Entrenamiento de cuerpo completo Ejercicios: Sentadillas, Press de banca, Peso muerto",
+        2: "Día 1: Parte superior del cuerpo Ejercicios: Press de banca, Dominadas, Fondos Día 2 : Parte inferior del cuerpo Ejercicios: Sentadillas, Peso muerto, Estocadas",
+        3: "Día 1: Pecho y espalda Ejercicios: Press de banca, Remo con barra, Pull-ups Día 2: Piernas Ejercicios: Sentadillas, Peso muerto, Extensiones de pierna Día 3: Brazos y hombros Ejercicios: Curl de bíceps, Press militar, Elevaciones laterales",
+        4: "Dia 1: Pecho Ejercicios: Press de banca, Banco inclinado con mancuernas, Pectolera Día 2: Espalda Ejercicios: Remo con barra, Jalon al pecho, Pullover Día 3: Piernas Ejercicios: Sentadilla con barra, Peso muerto, Extension de cuadriceps Día 4: Zona media y cardio Ejercicios: Plancha, Crunches, Espinales, 20 mins de cinta",
+        5: "Dia 1: Piernas anterior Ejercicios: Sentadilla con barra, Sentadilla búlgara, Extension de cuadriceps Día 2: Pecho Ejercicios: Press de banca, Banco inclinado con mancuernas, Pectolera Día 3: Espalda Ejercicios: Remo con barra, Jalon al pecho Día 4: Piernas posterior Ejercicios: Peso muerto, Balanza unilateral, Maquina isquios, Gemelos Dia 5:Zona media y cardio Ejercicios: Plancha, Crunches, Espinales, 20 mins de cinta",
+        6: "Dia 1: Piernas anterior Ejercicios: Sentadilla con barra, Sentadilla búlgara, Extension de cuadriceps Día 2: Pecho Ejercicios: Press de banca, Banco inclinado con mancuernas, Pectolera Día 3: Espalda Ejercicios: Remo con barra, Jalon al pecho Día 4: Piernas posterior Ejercicios: Peso muerto, Balanza unilateral, Maquina isquios, Gemelos Dia 5:Zona media y cardio Ejercicios: Plancha, Crunches, Espinales, 20 mins de cinta Día 6: Brazos Ejercicios: Biceps martillo, Triceps en polea, Biceps en banco scott, Press francés",
+        7:"Dia 1: Piernas anterior Ejercicios: Sentadilla con barra, Sentadilla búlgara, Extension de cuadriceps Día 2: Pecho Ejercicios: Press de banca, Banco inclinado con mancuernas, Pectolera Día 3: Espalda Ejercicios: Remo con barra, Jalon al pecho Día 4: Piernas posterior Ejercicios: Peso muerto, Balanza unilateral, Maquina isquios, Gemelos Dia 5:Zona media y cardio Ejercicios: Plancha, Crunches, Espinales, 20 mins de cinta Día 6: Brazos Ejercicios: Biceps martillo, Triceps en polea, Biceps en banco scott, Press francés Día 7: Flexibilidad Ejercicios: Estiramiento unilateral de gluteos, cuadriceps e isquios, Gato contento/enojado, Estiramientos de torso completo"
+      };
+    
+    var dias = parseInt(document.getElementById('dias').value);
+    
+    var rutinaTexto = rutinas[dias];
+    
+    var rutinaElement = document.getElementById('rutina');
+    rutinaElement.textContent = rutinaTexto;
+    }
   
 
 
